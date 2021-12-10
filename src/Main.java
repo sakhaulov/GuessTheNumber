48ddae6d30f95e4e7af2a3ce61ae3c0a3a62b002
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2021-12-10 12:27:15 +0500

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Ваша задача - угадать число.");
+        for (int i = 10; i <= 30; i += 10) playLevel(i);
+        System.out.println("Вы выиграли!");
+        scanner.close();
+    }
+
+    private static void playLevel(int range) {
+        int number = (int)(Math.random() * range);
+        while (true){
+            System.out.println("Угадайте число от 0 до "+ range);
+            int input_number = scanner.nextInt();
+            if (input_number == number) {
+                System.out.println("Вы угадали.");
+                break;
+            } else if (input_number < number) {
+                System.out.println("Ввведённое число меньше.");
+            } else {
+                System.out.println("Введённое число больше.");
+            }
+        }
+    }
+}
